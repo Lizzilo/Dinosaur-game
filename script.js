f330c98692ca2fa74b3855098de4652bcd7dad76
Liza Odish <lizaodishl@gmail.com>
2023-02-21 15:52:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var checkDead = setInterval(function() {
     parseInt (window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft =
     parseInt (window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>0 %% characterTop>=130) {
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130) {
         block.style.animation = "none";
         block.style.display = "none";
         alert("U Loose =(");
