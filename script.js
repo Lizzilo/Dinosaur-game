097c70048e60cb3d6c5a9603bcf7befcb8bddd98
Liza Odish <lizaodishl@gmail.com>
2023-02-21 15:18:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ var charachter =
 document.getElementById("block");
 function jump() {
     charachter.classList.add("animte");
+    setTimeout(function(){
+        charachter.classList.remove("animate");
+    },500) ;
 }
